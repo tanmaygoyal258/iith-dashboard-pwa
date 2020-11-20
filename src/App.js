baf3f203a82429afc8c585bb17726a02ca471753
butter-chicken27 <agraj.srivastava60@gmail.com>
2020-11-20 10:27:14 +0530

    User auth dummy done

--- src/App.js
+++ src/App.js
@@ -1,14 +1,32 @@
 import React from 'react';
 import './App.css';
-import { HashRouter, Route, Switch } from 'react-router-dom';
+import firebase from 'firebase';
+import firebaseConfig from './firebaseConfig';
 
 function App() {
+  firebase.initializeApp(firebaseConfig);
+  const googleProvider = new firebase.auth.GoogleAuthProvider();
+
   return (
-    <HashRouter basename="/">
-      <Switch>
-        <Route exact path="mess-menu" />
-      </Switch>
-    </HashRouter>
+    <button
+      type="submit"
+      onClick={() => {
+        const provider = googleProvider;
+        provider.addScope('profile');
+        provider.addScope('email');
+        firebase
+          .auth()
+          .signInWithPopup(provider)
+          .then((result) => {
+            const token = result.credential.accessToken;
+            const { user } = result;
+            console.log({ user });
+            console.log({ token });
+          });
+      }}
+    >
+      Sign in with google
+    </button>
   );
 }
 
