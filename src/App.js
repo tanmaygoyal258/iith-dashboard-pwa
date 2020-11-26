2476d5b00fbfc0c5200e4afe8b0bd128a9997cd8
Tarun Ram <ai19btech11004@iith.ac.in>
2020-11-26 23:33:20 +0530

    Removed firebaseConfig.js, added .env config

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import firebase from 'firebase';
+import dotenv from 'dotenv';
 import { useAuthState } from 'react-firebase-hooks/auth';
 import {
   BottomNavigation,
@@ -12,12 +13,24 @@ import RestaurantIcon from '@material-ui/icons/Restaurant';
 import LocalTaxiIcon from '@material-ui/icons/LocalTaxi';
 import DirectionsBusIcon from '@material-ui/icons/DirectionsBus';
 import CalendarTodayIcon from '@material-ui/icons/CalendarToday';
-import firebaseConfig from './firebaseConfig';
 import Mess from './pages/Mess';
 
 import './App.css';
 
-firebase.initializeApp(firebaseConfig);
+dotenv.config();
+
+console.log(process.env);
+
+firebase.initializeApp({
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
+});
+
 const googleProvider = new firebase.auth.GoogleAuthProvider();
 
 const login = () => {
