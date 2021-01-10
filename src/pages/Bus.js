df22f8eac123139eddb0dbf58e590b01c04e9464
butter-chicken27 <agraj.srivastava60@gmail.com>
2021-01-10 05:49:55 +0530

    Bus page

--- src/pages/Bus.js
+++ src/pages/Bus.js
@@ -1,6 +1,50 @@
-import React from 'react';
+import React, { useState } from 'react';
+import Button from '@material-ui/core/Button';
+import MenuItem from '@material-ui/core/MenuItem';
+import Select from '@material-ui/core/Select';
 
 function Bus() {
-  return <h1>Bus Page</h1>;
+  const [location, setLocation] = useState('LAB');
+  const [week, setWeek] = useState('Weekday');
+  const [open, setOpen] = React.useState(false);
+  const toggleWeek = () => {
+    if (week === 'Weekday') setWeek('Weekend');
+    else {
+      setWeek('Weekday');
+    }
+  };
+  const handleClose = () => {
+    setOpen(false);
+  };
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  const handleChange = (event) => {
+    setLocation(event.target.value);
+  };
+  return (
+    <div>
+      <Button color="primary" variant="contained" onClick={() => toggleWeek()}>
+        {week}
+      </Button>
+      <Select
+        labelId="demo-controlled-open-select-label"
+        id="demo-controlled-open-select"
+        open={open}
+        onClose={handleClose}
+        onOpen={handleOpen}
+        value={location}
+        onChange={handleChange}
+      >
+        <MenuItem value="LINGAMPALLY">Lingampally</MenuItem>
+        <MenuItem value="SANGAREDDY">Sangareddy</MenuItem>
+        <MenuItem value="ODF">ODF</MenuItem>
+        <MenuItem value="LAB">Maingate</MenuItem>
+      </Select>
+      <h1>{location}</h1>
+    </div>
+  );
 }
 export default Bus;
