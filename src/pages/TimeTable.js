d65c712b3ca361766a5fd11cd72153767db10d7c
Tarun Ram <ai19btech11004@iith.ac.in>
2021-02-02 16:43:35 +0530

    Added basic calendar to timetable page

--- src/pages/TimeTable.js
+++ src/pages/TimeTable.js
@@ -1,6 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { Calendar, momentLocalizer } from 'react-big-calendar';
+import moment from 'moment';
+import 'react-big-calendar/lib/css/react-big-calendar.css';
+
+const localizer = momentLocalizer(moment);
 
 function TimeTable() {
-  return <h1>TimeTable Page</h1>;
+  const eventList = useState([
+    {
+      start: moment().toDate(),
+      end: moment().add(1, 'days').toDate(),
+      title: 'Some title',
+    },
+  ]);
+
+  return (
+    <Calendar
+      localizer={localizer}
+      events={eventList}
+      startAccessor="start"
+      endAccessor="end"
+      style={{ height: 500 }}
+    />
+  );
 }
 export default TimeTable;
