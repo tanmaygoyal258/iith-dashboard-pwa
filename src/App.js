d71f5820eb128f650f3d2eea6ce38f66f8bf52c6
butter-chicken27 <agraj.srivastava60@gmail.com>
2021-04-06 23:58:16 +0530

    Added a space

--- src/App.js
+++ src/App.js
@@ -46,6 +46,20 @@ const muiTheme = createMuiTheme({
       contrastText: '#FFFFFF',
     },
     background: {},
+    typography: {
+      fontFamily: [
+        '-apple-system',
+        'BlinkMacSystemFont',
+        '"Segoe UI"',
+        'Roboto',
+        '"Helvetica Neue"',
+        'Arial',
+        'sans-serif',
+        '"Apple Color Emoji"',
+        '"Segoe UI Emoji"',
+        '"Segoe UI Symbol"',
+      ].join(','),
+    },
   },
 });
 
